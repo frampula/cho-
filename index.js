ef9d4b8199412431af93e2d18d8f75af42aa377f
framupla <kidalo1337@gmail.com>
2024-01-05 18:38:22 +0200

    Function Dog(name, color, age)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function factorial(n) {
-  var res = 1;
-  while (n) res *= n--;
-  return res;
+function Dog(name, color, age) {
+  this.name = name;
+  this.color = color;
+  this.age = age;
+  this.gav = function () {
+    return this.name + ' gav-gav-gav';
+  };
 }
-console.log(factorial(5));
+
+const dog1 = new Dog("Tuzik", "Black", 1);
+
+const dog2 = new Dog("Knopa", "White", 3);
+
+const dog3 = new Dog("Liem", "Black", 3);
