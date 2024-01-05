27357d734b5b1af880ae107b92556e888e34b3bd
framupla <kidalo1337@gmail.com>
2024-01-05 20:25:58 +0200

    Task Resolves (Acceleration & Country)

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-function Book(title, author, year, price) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-  this.price = price;
+function Country(countryName, population, area) {
+  this.countryName = countryName;
+  this.population = population;
+  this.area = area;
 
-  this.getTitle = function () {
-    return this.title;
-  };
-
-  this.getAuthor = function () {
-    return this.author;
-  };
-
-  this.getYear = function () {
-    return this.year;
-  };
-
-  this.getPrice = function () {
-    return this.price;
-  };
-
-  this.setTitle = function (newTitle) {
-    this.title = newTitle;
-  };
-
-  this.setAuthor = function (newAuthor) {
-    this.author = newAuthor;
-  };
-
-  this.setYear = function (newYear) {
-    this.year = newYear;
-  };
-
-  this.setPrice = function (newPrice) {
-    this.price = newPrice;
-  };
-
-  this.calculateDiscountedPrice = function (discountPercentage) {
-    const discountedPrice =
-      this.price - this.price * (discountPercentage / 100);
-    return discountedPrice;
+  this.getDensity = function () {
+    return this.population / this.area;
   };
 }
 
-const book1 = new Book("Чотири вітри", "Крістін Генна", 2020, 450);
+let country1 = new Country("Bulgaria", 900000, 400000);
 
-console.log(book1.getTitle());
-console.log(book1.getYear());
-console.log(book1.calculateDiscountedPrice(90));
+/* ---------------------------------------------------- */
 
-book1.setTitle("New title");
+function Auto(name, maxSpeed) {
+  this.name = name;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
+
+  this.accelerate = function (accelValue) {
+    this.speed = this.speed + accelValue;
+    if (this.speed > this.maxSpeed) {
+      this.speed = maxSpeed;
+    }
+
+    return this.speed;
+  };
+
+  this.deaccelerate = function (deaccelValue) {
+    this.speed = this.speed - deaccelValue;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+
+    return this.speed;
+  };
+
+  this.stop = function () {
+    this.speed = 0;
+    return this.speed;
+  };
+}
+
+const auto1 = new Auto("Audi", 300);
