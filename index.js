3a30b70778b967dfe1d170a32d3eebccb9a47568
framupla <kidalo1337@gmail.com>
2024-01-08 19:50:54 +0200

    For in

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
-function Country(countryName, population, area) {
-  this.countryName = countryName;
-  this.population = population;
-  this.area = area;
+// const user = {
+//     name: 'John',
+//     lastName: 'Doe',
+//     age: 20,
+//     'favouirite color': 'red'
+// }
 
-  this.getDensity = function () {
-    return this.population / this.area;
-  };
+
+// for (let key in user) {
+//     console.log(`${key} ---> ${user[key]}`)
+// }
+
+const obj = {
+    key1: 'Izi',
+    key2: 'Pizi',
+    key3: 'Lemon',
+    key4: 'Squezy'
 }
 
-let country1 = new Country("Bulgaria", 900000, 400000);
-
-/* ---------------------------------------------------- */
-
-function Auto(name, maxSpeed) {
-  this.name = name;
-  this.maxSpeed = maxSpeed;
-  this.speed = 0;
-
-  this.accelerate = function (accelValue) {
-    this.speed = this.speed + accelValue;
-    if (this.speed > this.maxSpeed) {
-      this.speed = maxSpeed;
+function voicetag () {
+    for (let key in obj) {
+        console.log(` '${key}': *${obj[key]}*`)
     }
-
-    return this.speed;
-  };
-
-  this.deaccelerate = function (deaccelValue) {
-    this.speed = this.speed - deaccelValue;
-    if (this.speed < 0) {
-      this.speed = 0;
-    }
-
-    return this.speed;
-  };
-
-  this.stop = function () {
-    this.speed = 0;
-    return this.speed;
-  };
 }
 
-const auto1 = new Auto("Audi", 300);
+console.log(voicetag ())
