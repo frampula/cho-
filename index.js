34d3ca78fb9d20211ec2b681ddc3ca26e9318c81
framupla <kidalo1337@gmail.com>
2024-01-09 19:28:44 +0200

    ARRAY PEREMOGA

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let arr = [3, 6, 8, 2, 3, 5, 1]
-let res = 0
-function sum(arrSum) {
-    for(let i = 0; i < arrSum.length; i++) {
-        res += arrSum[i]
+let arr = [5, 6, 8, 2, 3, 4, 1]
+function biggest(array) {
+    let res = 0
+   for (let i = 0; i < array.length; i++) {
+    if (res < array[i]) {
+        res = array[i]
     }
-    return res
+   }
+   return res
 }
 
-
-console.log(sum(arr))
+console.log(biggest(arr))
