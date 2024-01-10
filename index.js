12bc42176deabad12d606d2619baed657d9b698a
framupla <kidalo1337@gmail.com>
2024-01-10 23:07:25 +0200

    Help from roma

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
-let arr = [5, 6, 8, 2, 3, 4, 1]
-function biggest(array) {
-    let res = 0
-   for (let i = 0; i < array.length; i++) {
-    if (res < array[i]) {
-        res = array[i]
-    }
-   }
-   return res
-}
+// function array (a,b) {
+//   for (let i = a; i <= b; i++ ) {
+//     console.log(i)
+//   }
+// }
 
-console.log(biggest(arr))
+// array (1,15)
+
+// function array (a,b) {
+//   for (let i = a; i <= b; i++ ) {
+//     console.log(i**2)
+//   }
+// }
+
+// array (1,15)
+
+// function array (a,b) {
+//   let sum = 0
+//   for (let i = a; i <= b; i++ ) {
+//     sum += i
+//   }
+//   return sum
+// }
+
+// console.log(array (1,100))
+
+// let student = {
+//   name: "John",
+//   age: 90,
+//   course: 1,
+// };
+
+// function stud(object) {
+//   return `His name ${object.name}, his age ${object.age},
+//   his course ${object.course}`;
+// }
+
+// console.log(stud(student))
+
+// console.log('John' in student)
+
+
+
+// function findKey (key, object) {
+//   for (currentKey in object) {
+//     if (key === currentKey) {
+//       return true
+//     }
+//   }
+//   return false
+// }
+
+// console.log(findKey ('age', student))
