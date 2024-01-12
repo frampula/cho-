d253cd95d9f64e9e841a9fd3a8b880b6c9d5bf47
framupla <kidalo1337@gmail.com>
2024-01-12 18:20:05 +0200

    array[i] === array[i+1]

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const arr1 = [1, 2, 3]
-const arr2 = [4, 5, 6]
-const arr3 = [3, 4, 5]
-const arr4 = ['aaa', 'bbb', 'ccc']
-const arr5 = ['3', 7, 10]
-arr1.concat(arr2)
-
-console.log(arr1.concat(arr2))
-
-
-
-console.log(arr1.reverse())
-
-arr3.push(4, 8, 10)
-
-console.log(arr3)
-
-
-
-console.log(arr4.pop())
-
-arr5.unshift(2, 8)
-
-console.log(arr5)
-
-console.log(arr5.join())
+function isDoubleElement(array) {
+    for(let i = 0; i < array.lengt; i++) {
+        array[i] === array[i+1]
+        return true
+    }
+    return false
+}
