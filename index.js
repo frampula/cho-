6cdc6ad54dbde6478ef998c0e344a9a20b8f5fda
framupla <kidalo1337@gmail.com>
2024-01-18 19:29:42 +0200

    array1.reduce

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function isDoubleElement(array) {
-    for(let i = 0; i < array.lengt; i++) {
-        array[i] === array[i+1]
-        return true
-    }
-    return false
+"use strict";
+
+const arrowSum = (...restArray) => {
+  let sum = restArray.reduce ((accumulator, currentValue) => {
+    return accumulator + currentValue
+  })
+  return sum
 }
+
+console.log(arrowSum(15,15))
+
+// "use strict";
+
+// const arrowSum = (...restArray) => {
+//   return restArray.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue,
+//     0
+//   );
+// };
+
+// console.log(arrowSum(15, 15));
