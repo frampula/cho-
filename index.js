bff7c0fae2257cfb181f45779fd5934a5d90b1c0
framupla <kidalo1337@gmail.com>
2024-02-06 19:49:30 +0200

    Destructure

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-class Fuel {
-  constructor(volume, density) {
-    this.volume = volume;
-    this.density = density;
-  }
-
-  getWeight() {
-    return this.volume * this.density;
-  }
+function getFullName({firstName, lastName, ...restObject}) {
+    console.log(restObject)
+  return `${firstName} ${lastName}`;
 }
 
-const gas = new Fuel(50, 0.9);
-console.log(gas.getWeight());
+const user2 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 42,
+  geolocation: "42.234423423 56.1123142154",
+  browser: "Chrome",
+};
 
-class Auto {
-  constructor(name, ownWeight, fuel) {
-    this.name = name;
-    this.ownWeight = ownWeight;
-    this.fuel = fuel;
-  }
-
-  getFullWeight() {
-    return this.ownWeight + this.fuel.getWeight()
-  }
-}
-const BMW = new Auto('BMW', 4000, gas)
-
-console.log(BMW.getFullWeight())
+console.log(getFullName(user2));
